425c788681fab690bd95e0e4e0ea6959223179cb
emanherawy <eman.herawy@gmail.com>
2022-01-18 16:15:43 +0200

    deploy latest updates to bsc test net

--- deploy/1_stakes.deploy.js
+++ deploy/1_stakes.deploy.js
@@ -4,12 +4,7 @@ const { parseEther } = require('ethers/lib/utils')
 module.exports = async ({ getNamedAccounts, deployments }) => {
   const { deploy } = deployments
   const { deployer } = await getNamedAccounts()
-  const _maxSupply = 8888
-  const dayinSec = 60 //* 60 * 24;
-  const _lockDuration = dayinSec*7 // 7 days
-  const _wallets = ['0x2819C6d61e4c83bc53dD17D4aa00deDBe35894AA']
-  const _mintPrice = parseEther((0.0001).toString())
-  const busd = "0xeD24FC36d5Ee211Ea25A80239Fb8C4Cfd80f12Ee"
+  const _lockDuration = 7//dayinSec*7 // 7 days
   const idoToken = "0xbf0ffba74bbfe340788a9d951d7b3a1a261e5eb2" // stfi
   const _owner = deployer
   
