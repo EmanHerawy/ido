96d6e7bb474a612df43d6b66a37d91790a6052e3
emanherawy <eman.herawy@gmail.com>
2022-01-19 09:45:31 +0200

    deploy to bsc with mainnet config but with 7 sec lock

--- deploy/2_ido.deploy.js
+++ deploy/2_ido.deploy.js
@@ -5,13 +5,14 @@ module.exports = async ({ getNamedAccounts, deployments }) => {
   const { deploy, get, execute } = deployments
   const { deployer } = await getNamedAccounts()
   const _maxSupply = parseEther("50000")
-  const _startTimeSale = 1640704213//Date.now()
-  const _wallets = ['0x2819C6d61e4c83bc53dD17D4aa00deDBe35894AA']
-  const _mintPrice = parseEther((0.0001).toString())
-  const busd = "0xeD24FC36d5Ee211Ea25A80239Fb8C4Cfd80f12Ee"
-  const _owner = deployer
-  const idoToken = "0xbf0ffba74bbfe340788a9d951d7b3a1a261e5eb2" // stfi
-  const _lockDuration = 7//dayinSec*7 // 7 days
+  const _startTimeSale = 1642542154//Date.now()
+  const _wallets = ['0x3Df593fBF718917582fF35E40C874f3B0d1E86c6']
+  const _mintPrice = parseEther((0.00567).toString())
+  const busd = '0xeD24FC36d5Ee211Ea25A80239Fb8C4Cfd80f12Ee'//'0xeD24FC36d5Ee211Ea25A80239Fb8C4Cfd80f12Ee' // main 0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56
+  const _owner = deployer// 0xac701BB1557F6c06070Bc114Ca6Ace4a358D3A84
+  const idoToken = "0xbf0ffba74bbfe340788a9d951d7b3a1a261e5eb2" // stfi // bsc mainnet 0x04ae5cb48B8f968ED821972c5480b5B850f55554
+  const dayInSec = 86400;
+  const _lockDuration =7 //dayInSec*7 // 7 days
   const startFiIDO = await deploy('AirdropedStartfiIDOWithStaking', {
     from: deployer,
     args: [
