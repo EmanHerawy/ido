c7ace3573ea7e27727a58bd7a41304087ef5fe17
emanherawy <eman.herawy@gmail.com>
2022-01-20 19:13:14 +0200

    MVPad ido private

--- deploy/2_ido.deploy.js
+++ deploy/2_ido.deploy.js
@@ -4,10 +4,10 @@ const { parseEther } = require('ethers/lib/utils')
 module.exports = async ({ getNamedAccounts, deployments }) => {
   const { deploy, get, execute } = deployments
   const { deployer } = await getNamedAccounts()
-  const _maxSupply = parseEther("100000")
+  const _maxSupply = parseEther("8680556") // mvpad 7,812,500
   const _startTimeSale = 1642542154//Date.now()
   const _wallets = ['0x3Df593fBF718917582fF35E40C874f3B0d1E86c6']
-  const _mintPrice = parseEther((0.001).toString())
+  const _mintPrice = parseEther((0.00576).toString())
   const busd = '0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56'//'0xeD24FC36d5Ee211Ea25A80239Fb8C4Cfd80f12Ee' // main 0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56
   const _owner = deployer// 0xac701BB1557F6c06070Bc114Ca6Ace4a358D3A84
   const idoToken = "0x04ae5cb48B8f968ED821972c5480b5B850f55554" // stfi // bsc // test 0xbf0ffba74bbfe340788a9d951d7b3a1a261e5eb2 mainnet 0x04ae5cb48B8f968ED821972c5480b5B850f55554
